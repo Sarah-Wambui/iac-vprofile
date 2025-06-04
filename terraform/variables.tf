fc4f9ccf36187c13b3b3aacb25e2cf09bfc09d1a
sarahwambui <sarahwambui843@gmail.com>
2025-06-04 14:04:30 +0300

    Fixed: Update GitHub Actions workflow and Terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,10 +10,5 @@ variable "clusterName" {
   type        = string
   default     = "vprofile-eks"
 }
-# Dummy variable to trigger GitHub Actions
-variable "trigger_temp" {
-  description = "Temporary variable to trigger workflow"
-  type        = string
-  default     = "temp"
-}
+
 
